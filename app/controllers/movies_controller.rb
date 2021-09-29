e0129f935c87a1ed895b74b74a1cf2adcd58120b
EC2 Default User <ec2-user@ip-172-31-30-103.us-east-2.compute.internal>
2021-09-29 20:11:10 +0000

    part2&3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,36 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all.order(params[:sort])
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @checked_ratings = params[:ratings] || session[:ratings]
+    @sort = params[:sort] || session[:sort]
+    @checkbox = {}
+    
+    if !@checked_ratings
+      session[:ratings] = {}
+      @all_ratings.each do |rating|
+        session[:ratings][rating] = 1
+      end
+      @checked_ratings = session[:ratings]
+    end
+    
+    if !(params[:sort] == session[:sort] && params[:ratings] == session[:ratings])
+      params[:sort] = session[:sort] = @sort
+      params[:ratings] = session[:ratings] = @checked_ratings
+      flash.keep
+      redirect_to movies_path(:sort=>params[:sort], :ratings =>params[:ratings])
+    end
+    
+    @all_ratings.each do |rating|
+      @checkbox[rating] = !@checked_ratings.nil? && @checked_ratings.keys.include?(rating)
+    end
+    session[:sort] = @sort
+    session[:ratings] = @checked_ratings
+
+    @movies = Movie.order @sort
+    if @checked_ratings
+      @movies = Movie.where(:rating => @checked_ratings.keys).order @sort
+    end
     
   end
 
